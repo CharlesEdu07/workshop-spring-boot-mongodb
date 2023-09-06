58c8f77c9d4306309ae3627f9a4ea385481a10ff
unknown <charleseduardofaria@gmail.com>
2023-09-06 11:49:27 -0300

    Criando UserRepository e UserService

--- src/main/java/com/charlesedu/workshopmongo/resources/UserResource.java
+++ src/main/java/com/charlesedu/workshopmongo/resources/UserResource.java
@@ -1,28 +1,26 @@
 package com.charlesedu.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.charlesedu.workshopmongo.domain.User;
+import com.charlesedu.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User user1 = new User("1", "Hally", "hally@gmail.com");
-        User user2 = new User("2", "Dudu", "dudu@gmail.com");
-
-        List<User> list = new ArrayList<>();
-
-        list.addAll(Arrays.asList(user1, user2));
+        List<User> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
     }
